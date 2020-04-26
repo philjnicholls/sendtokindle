89de4421b44eae74c991fb21cc212c074d7b4da1
Phil Nicholls <phil.j.nicholls@gmail.com>
2020-04-26 11:00:26 +0300

    Added flasj and played a bit with trying to get the title of the doc on Kindle to show.

--- sendtokindle.py
+++ sendtokindle.py
@@ -2,15 +2,27 @@ import requests
 import smtplib
 import ssl
 import configparser
+import re
+import flask
 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase 
-from email import encoders 
+from email.mime.base import MIMEBase
+from email import encoders
 from bs4 import BeautifulSoup
+from flask import request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+ATTRIBUTE_BLACKLIST = ['style']
+TAG_BLACKLIST = ['script', 'style']
 
 URL = 'https://realpython.com/python-testing/'
-#URL = 'https://www.ladybirdeducation.co.uk/the-importance-of-fairy-tales-in-the-efl-classroom/'
+'''
+URL = ('https://www.ladybirdeducation.co.uk/'
+            'the-importance-of-fairy-tales-in-the-efl-classroom/')
+'''
 
 '''
 __author__ = "Phil Nicholls"
@@ -28,86 +40,126 @@ a Kindle for easy reading on the eyes
 '''
 
 
-page = requests.get(URL, allow_redirects=True)
+@app.route('/', methods=['GET'])
+def send_page_to_kindle():
+    if 'url' in request.args:
+        url = request.args['url']
+    else:
+        url = URL
+    page = requests.get(url, allow_redirects=True)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-title = soup.find('title')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-'''
-Try to get a main HTML element which should have the
-main body of the webpage
-'''
+    # Strip the styling and unwanted from the page
+    for tag in soup.findAll():
+        if tag.name.lower() in TAG_BLACKLIST:
+            del tag
+        # TODO Strip attributes
 
-paths_to_main = (
-    {
-        'element': 'main',
-    },
-    {
-        'element': 'article',
-    },
-    {
-        'element': 'div',
-        'class': 'main-content',
-    },
-)
+    title = soup.find('title').text
 
-for path in paths_to_main:
-    main = soup.find(path['element'],
-        class_=path['class'] if 'class' in path else None
-    )    
-    if main:
-        break
+    '''
+    Try to get a main HTML element which should have the
+    main body of the webpage
+    '''
 
-config = configparser.ConfigParser()
-config.read('.sendtokindle.rc')
-
-sender_email = config['SMTP']['EMAIL']
-if 'USERNAME' in config['SMTP']:
-    username = config['SMTP']['USERNAME']
-else:
-    username = sender_email
-password = config['SMTP']['PASSWORD']
-host = config['SMTP']['HOST']
-port = config['SMTP']['PORT']
-kindle_email = config['Kindle']['EMAIL']
-
-# TODO Strip down the HTML to create a plain text version of the page
-body_text = 'Just a test.'
-body_html = str(main)
-
-part1 = MIMEText(body_text, "plain")
-part2 = MIMEText(body_html, "html")
-
-message = MIMEMultipart("alternative")
-message["Subject"] = title.text
-message["From"] = sender_email
-message["To"] = kindle_email
-
-message.attach(part1)
-message.attach(part2)
-
-# open the file to be sent  
-filename = title.text + '.html'
-
-# instance of MIMEBase and named as p 
-p = MIMEBase('application', 'octet-stream') 
-  
-# To change the payload into encoded form 
-p.set_payload(body_html) 
-  
-# encode into base64 
-encoders.encode_base64(p) 
-   
-p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
-  
-# attach the instance 'p' to instance 'msg' 
-message.attach(p) 
-
-
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(
-        sender_email, kindle_email, message.as_string()
+    paths_to_main = (
+        {
+            'element': 'main',
+        },
+        {
+            'element': 'article',
+        },
+        {
+            'element': 'div',
+            'class': 'main-content',
+        },
+        {
+            'element': 'div',
+            'id': 'main',
+        },
+        {
+            'element': 'div',
+            'id': 'content',
+        },
     )
-    server.quit()
+
+    # Search the HTML for the main content
+    for path in paths_to_main:
+        main = soup.find(
+            path['element'],
+            class_=path['class'] if 'class' in path else None,
+            id_=path['id'] if 'id' in path else None
+        )
+        if main:
+            break
+
+    config = configparser.ConfigParser()
+    config.read('.sendtokindle.rc')
+
+    sender_email = config['SMTP']['EMAIL']
+    if 'USERNAME' in config['SMTP']:
+        username = config['SMTP']['USERNAME']
+    else:
+        username = sender_email
+    password = config['SMTP']['PASSWORD']
+    host = config['SMTP']['HOST']
+    port = config['SMTP']['PORT']
+    kindle_email = config['Kindle']['EMAIL']
+
+    '''
+    TODO Strip down the HTML to create a plain
+    text version of the page
+    '''
+    body_text = 'Just a test.'
+    html_file = '''<html>
+        <head>
+            <title>''' + title + '''</title>
+            <meta http-equiv="Content-Type"
+                content="text/html; charset=UTF-8" />
+        </head>
+        <body>''' + str(main) + '''</body>
+    </html>'''
+    body_html = html_file
+
+    part1 = MIMEText(body_text, "plain")
+    part2 = MIMEText(body_html, "html")
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = title
+    message["From"] = sender_email
+    message["To"] = kindle_email
+
+    message.attach(part1)
+    message.attach(part2)
+
+    # open the file to be sent
+    filename = title + '.html'
+
+    # instance of MIMEBase and named as p
+    p = MIMEBase('application', 'octet-stream')
+
+    # To change the payload into encoded form
+    p.set_payload(body_html)
+
+    # encode into base64
+    encoders.encode_base64(p)
+
+    title_stripped = re.sub('[^A-Za-z0-9 ]+', '', title)
+    p.add_header(
+        'Content-Disposition',
+        'attachment; filename= %s' % title_stripped + '.html')
+
+    # attach the instance 'p' to instance 'msg'
+    message.attach(p)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, kindle_email, message.as_string()
+        )
+        server.quit()
+
+
+app.run()
