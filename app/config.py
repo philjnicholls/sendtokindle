3daa2cd005a1b40fb2a80c5a582be3309dd6fea3
Phil Nicholls <phil.j.nicholls@gmail.com>
2020-05-04 15:48:51 +0300

    Moved MySQL config to environment variable.

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 import os
+from os import environ
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql://sendtokindle:SEMan4n@@localhost/sendtokindle'
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
