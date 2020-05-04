7b099ffab3c623473f1f8ba5eb223d839d21fef5
Phil Nicholls <phil.j.nicholls@gmail.com>
2020-05-04 17:10:05 +0300

    Get SQL connection from conf file.

--- app/config.py
+++ app/config.py
@@ -1,8 +1,21 @@
 import os
+import requests
+import configparser
+
 from os import environ
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    if os.path.exists(os.path.join(BASE_DIR, 'mysql.cnf')):
+        config = configparser.ConfigParser()
+        config.read(os.path.join(BASE_DIR, 'mysql.cnf'))
+    else:
+        raise requests.exceptions.RequestException('Missing database config.', 404)
+
+    SQLALCHEMY_DATABASE_URI = 'mysql://%s:%s@%s/%s' % (config['client']['user'],
+                                                       config['client']['password'],
+                                                       config['client']['host'],
+                                                       config['client']['database'])
     SQLALCHEMY_TRACK_MODIFICATIONS = False
