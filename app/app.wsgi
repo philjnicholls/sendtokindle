573d8e66533c7a131e71e5de3f239d2b5c4ff4b8
Phil Nicholls <phil.j.nicholls@gmail.com>
2020-05-06 10:21:52 +0300

    Tidy up and refactor.

--- app/app.wsgi
+++ app/app.wsgi
@@ -2,13 +2,11 @@ import logging
 import sys
 import os
 
+from app.config import BASE_DIR
+
 logging.basicConfig(stream=sys.stderr)
 sys.stdout = sys.stderr
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0, BASE_DIR)
 from app import app as application
 application.secret_key = 'gs&&6nu$^hy-uowy*$0s3an%fxr21g2t41ofle!6i97hxx#7ji'
--- app/routes.py
+++ app/routes.py
@@ -23,9 +23,7 @@ from app import db
 from app.models import User
 from app.forms import RegisterForm
 from app.extensions import csrf
-
-# Project directory for file access
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+from app.config import BASE_DIR
 
 '''
 __author__ = "Phil Nicholls"
@@ -47,12 +45,19 @@ Runs as a Flask REST API on a webserver of your choice
 
 @app.errorhandler(Exception)
 def handle_error(error):
+    '''
+    Catches errors from the app and tries to send back
+    a nice HTTP header and JSON response
+    :param error: The error that has been raised
+    :return:
+    '''
     message = [str(x) for x in error.args]
 
     status_code = None
     try:
         status_code = str(int(error.strerror))
-    except ValueError or TypeError or AttributeError:
+    except:
+        # Maybe strerror is not what I thought
         status_code = '500'
 
     if not status_code:
@@ -83,14 +88,37 @@ def check_arguments(values):
     # If no URL is specified, raise an error
     if 'url' in values:
         cleaned['url'] = request.values['url']
+    else:
+        raise requests.exceptions.RequestException('Missing parameter "url".', 400)
+
 
     if 'token' in values:
         cleaned['token'] = request.values['token']
+    else:
+        raise requests.exceptions.RequestException('Missing parameter "token".', 400)
 
     return cleaned
 
 
-def send_email(config, to_email, subject, html=None, plain_text=None, attachment_title=None, attachment_path=None):
+def send_email(to_email,
+               subject,
+               html=None,
+               plain_text=None,
+               attachment_title=None,
+               attachment_path=None):
+    '''
+    Sends an email using redentials from the .sendtokindle.rc file
+    :param to_email: Recipient
+    :param subject: Email subject
+    :param html: HTML body for the email
+    :param plain_text: Plain text body for the email
+    :param attachment_title: Name of the attachement as it will
+        appear on the email
+    :param attachment_path: Path to the attachement
+    :return:
+    '''
+
+    config = get_config()
 
     sender_email = config['SMTP']['EMAIL']
     if 'USERNAME' in config['SMTP']:
@@ -144,7 +172,20 @@ def send_email(config, to_email, subject, html=None, plain_text=None, attachment
         server.quit()
 
 
-def send_kindle_email(token, config, title, html, tmp_dir):
+def send_kindle_email(token, title, html, tmp_dir):
+    '''
+    Sends an email to a user matching the API token with an attached
+    mobi file
+
+    :param token: The api_token to lookup the user in the db
+    :param title: Title of the mobi article
+    :param html: The html to generate the mobi from
+    :param tmp_dir: Currently open directory to use for
+        mobi generation and source images
+    :return:
+    '''
+
+    # Run som e checks to make sure our user is valid
     user = User.query.filter_by(api_token=token).first()
     if not user:
         raise requests.exceptions.RequestException('No matching token found.', 401)
@@ -152,16 +193,14 @@ def send_kindle_email(token, config, title, html, tmp_dir):
     if not user.verified:
         raise requests.exceptions.RequestException('You have not verified your email adress.', 401)
 
-    '''
-        Add HTML tags to make a valid HTML doc
-        '''
+    # Add HTML tags to make a valid HTML doc
     html_file = '''<html>
             <head>
                 <title>''' + title + '''</title>
                 <meta http-equiv="Content-Type"
                     content="text/html; charset=UTF-8" />
             </head>
-            <body>''' + html + '''</body>
+            <body><h1>''' + title + '''</h1>''' + html + '''</body>
         </html>'''
 
     '''
@@ -175,8 +214,7 @@ def send_kindle_email(token, config, title, html, tmp_dir):
         mobi_path = os.path.splitext(temp_file.name)[0] + '.mobi'
         temp_file.close()
 
-    send_email(config=config,
-               to_email=user.kindle_email,
+    send_email(to_email=user.kindle_email,
                subject=title,
                attachment_path=mobi_path,
                attachment_title=os.path.basename(mobi_path))
@@ -191,43 +229,66 @@ def get_config():
         raise requests.exceptions.RequestException('Missing server config.', 404)
 
 
+def get_page(url):
+    '''
+    Download the page and parse
+    :param url: URL of the page to download
+    :return:
+    '''
+    # Setup some config for newspaper
+    config = Config()
+    config.keep_article_html = True
+    config.follow_meta_refresh = True
+
+    article = Article(url, config=config)
+    article.download()
+    article.parse()
+    article.fetch_images()
+
+    return article
+
+
+def dump_images(images, tmp_dir):
+    '''
+    Writes images to the supplied temporary directory so
+    they are ready for kindlgen to pick up
+    :param images: array of image URLs
+    :param tmp_dir: open temporary directory to store
+        the images
+    :return:
+    '''
+    for image in images:
+        os.makedirs(os.path.join(tmp_dir, os.path.dirname(image)), exist_ok=True)
+        try:
+            r = requests.get(image)
+        except:
+            r = None
+
+        if r:
+            with open(os.path.join(tmp_dir, image), 'wb') as f:
+                f.write(r.content)
+                f.close()
+
 @app.route('/api', methods=['POST'])
 @csrf.exempt
 def send_page_to_kindle():
+    '''
+    Public api which takes a token and url, finds the main page content
+    and emails it as a mobi file.
+    :return:
+    '''
     # Check for the required parameters
     args = check_arguments(request.values)
-    if 'url' not in args:
-        raise requests.exceptions.RequestException('Missing parameter "url".', 400)
 
-    np_config = Config()
-    np_config.keep_article_html = True
-    np_config.follow_meta_refresh = True
-
-    # Get the page
-    article = Article(args['url'], config=np_config)
-    article.download()
-    article.parse()
+    article = get_page(args['url'])
 
     if article.article_html:
-        s2k_config = get_config()
-        article.fetch_images()
 
         # Dump the images in tmp for kindlegen
         with tempfile.TemporaryDirectory() as tmp_dir:
-            for image in article.images:
-                os.makedirs(os.path.join(tmp_dir, os.path.dirname(image)), exist_ok=True)
-                try:
-                    r = requests.get(image)
-                except:
-                    r = None
-
-                if r:
-                    with open(os.path.join(tmp_dir, image), 'wb') as f:
-                        f.write(r.content)
-                        f.close()
+            dump_images(article.images, tmp_dir)
 
             send_kindle_email(token=args['token'],
-                              config=s2k_config,
                               title=article.title,
                               html=article.article_html,
                               tmp_dir=tmp_dir)
@@ -295,9 +356,7 @@ def home():
         db.session.commit()
 
         # Send email to validate email
-        config = get_config()
-        send_email(config=config,
-                   to_email=user.email,
+        send_email(to_email=user.email,
                    subject='Verify your email address',
                    plain_text='%sverify?token=%s&email=%s' % (request.url_root,
                                                               user.email_token,
