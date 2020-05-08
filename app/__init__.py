9d90127cb0147c3320b6fa3240f53d1aaa1924e5
Phil Nicholls <phil.j.nicholls@gmail.com>
2020-05-08 13:30:54 +0300

    Adding comments.

--- app/__init__.py
+++ app/__init__.py
@@ -12,11 +12,21 @@ from worker import conn
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
+
+# Enable CSRF protection for forms
 csrf.init_app(app)
+
+# Database engine
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Allow any domain to all the API
 CORS(app)
+
+# Used to catch errors and wrap with HTTP exceptions
 errors = Blueprint('errors', __name__)
+
+# Redis queue
 q = Queue(connection=conn)
 
 from app import routes, models
--- app/config.py
+++ app/config.py
@@ -9,6 +9,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    # Get the SQL connection settings from settings rc file
     if os.path.exists(os.path.join(BASE_DIR, '.sendtokindle.rc')):
         config = configparser.ConfigParser()
         config.read(os.path.join(BASE_DIR, '.sendtokindle.rc'))
