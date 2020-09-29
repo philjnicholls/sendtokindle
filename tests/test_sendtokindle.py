a41117b22544cf6c287eaa9ab6e0224d066716fd
Phil Nicholls <phil.j.nicholls@gmail.com>
2020-09-29 15:43:54 +0300

    Teaking requirements and tests to make flake8 happy

--- tests/test_sendtokindle.py
+++ tests/test_sendtokindle.py
@@ -10,20 +10,24 @@ __email__ = "phil.j.nicholls@gmail.com"
 __status__ = "Development"
 """
 
+from app import app
+from app.models import User
+
 import pytest
 
 from redis import Redis
-from rq import SimpleWorker, Queue
 
-from app import app
-from app.models import User
+from rq import Queue, SimpleWorker
 
 messages = []
 
 
 @pytest.fixture(autouse=True)
 def log_smtp(monkeypatch):
-    """Prevent email being sent with SMTP and log instead."""
+    """Prevent email being sent with SMTP and log instead.
+
+    :param monkeypatch: To override SMTP functions
+    """
     global messages
     monkeypatch.setattr('smtplib.SMTP.login', lambda *args,
                         **kwargs: (235,
@@ -34,15 +38,22 @@ def log_smtp(monkeypatch):
 
 
 @pytest.fixture
-def test_app(log_smtp):
-    """Returns a test app."""
+def test_app():
+    """Returns a test app.
+
+    :returns: F;lask test client
+    """
     app.config['WTF_CSRF_ENABLED'] = False
     return app.test_client()
 
 
 @pytest.fixture
 def api_token(log_smtp):
-    """Return a validated api token."""
+    """Return a validated api token.
+
+    :param log_smtp: Monkeypatch to stop SMTP emails and log
+    :return: Verified API stoken string for API access
+    """
     app.config['WTF_CSRF_ENABLED'] = False
     test_app = app.test_client()
     payload = {
@@ -57,7 +68,12 @@ def api_token(log_smtp):
 
 
 def test_send_webpages(test_app, api_token, log_smtp):
-    """Create RQ jobs for emailing webpages as articles."""
+    """Create RQ jobs for emailing webpages as articles.
+
+    :param test_app: App client
+    :param api_token: verified token for API access
+    :param log_smtp: Monkeypatch for logging SMTP emails
+    """
     global messages
     messages = []
     webpages = (
@@ -75,7 +91,7 @@ def test_send_webpages(test_app, api_token, log_smtp):
             'url': webpage,
         }
 
-        response = test_app.post('/api', data=payload)
+        response = test_app.post('/api', json=payload)
 
         assert response.json is not None
         assert type(response.json['success']) == bool
@@ -84,28 +100,40 @@ def test_send_webpages(test_app, api_token, log_smtp):
 
 
 def test_missing_url(test_app):
-    """Error as URL or HTML not supplied in payload."""
+    """Error as URL or HTML not supplied in payload.
+
+    :param test_app: App client
+    """
     payload = {}
 
-    response = test_app.post('/api', data=payload)
+    response = test_app.post('/api', json=payload)
 
     assert response.status_code == 400
 
 
 def test_bad_url(test_app, api_token):
-    """Error as supplied URL is not retrievable."""
+    """Error as supplied URL is not retrievable.
+
+    :param test_app: App client
+    :param api_token: verified token for API access
+    """
     payload = {
         'token': api_token,
         'url': 'http://dgdgjs.fff/dhdgdyu'
     }
 
-    response = test_app.post('/api', data=payload)
+    response = test_app.post('/api', json=payload)
 
     assert response.status_code == 500
 
 
 def test_queue(log_smtp, test_app, api_token):
-    """Run the RQ queue to extract pages and send emails."""
+    """Run the RQ queue to extract pages and send emails.
+
+    :param log_smtp: Monkeypatch for logging SMTP emails
+    :param test_app: App client
+    :param api_token: verified token for API access
+    """
     global messages
     messages = []
     payload = {
@@ -113,7 +141,7 @@ def test_queue(log_smtp, test_app, api_token):
         'url': 'https://realpython.com/flask-blueprint/',
     }
 
-    test_app.post('/api', data=payload)
+    test_app.post('/api', json=payload)
     queue = Queue(connection=Redis())
 
     assert len(queue.jobs) > 0
